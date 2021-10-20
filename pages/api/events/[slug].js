dc2043cfdf0830d99657e1ca82afabbe5ac33735
jwright90 <jason.wright90@gmail.com>
2021-10-20 09:32:21 +0100

    add aliases and use getStaticProps to fetch events data

--- pages/api/events/[slug].js
+++ pages/api/events/[slug].js
@@ -9,3 +9,4 @@ export default function handler(req, res) {
     res.status(405).json({message: `Method ${req.method} is not allowed`})
   }
 }
+
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,28 @@
-import Layout from '../components/Layout'
+import Layout from '@/components/Layout'
+import { API_URL } from '@/config/index'
 import Link from 'next/link'
 
 export default function HomePage() {
   return (
     <Layout>
-      <h1>Home Page</h1>
+      <h1>Upcoming Events</h1>
       <Link href="/about">About</Link>
    </Layout>
   )
 }
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`)
+  const events = await res.json()
+
+
+  return {
+    props: { events },
+    revalidate: 1 // If the data is changed then update after 1 second
+  }
+}
+
+  // getServerSideProps runs everytime we come to the page
+  // getStaticProps only runs on build time. If someone adds an event,
+  // it will not show when they reload the page as this doesn't run
+  // on every request
