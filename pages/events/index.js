cead4e5845a955db3d016abb1e4ed2de0884137c
jwright90 <jason.wright90@gmail.com>
2021-10-21 17:08:11 +0100

    add pagination to events

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,8 +1,12 @@
 import { API_URL } from '@/config/index'
 import EventItem from '@/components/EventItem'
 import Layout from '@/components/Layout'
+import Pagination from '@/components/Pagination'
 
-export default function HomePage({events}) {
+const PER_PAGE = 2
+
+export default function HomePage({ events, page, total }) {
+
   return (
     <Layout>
       <h1>Events</h1>
@@ -10,17 +14,28 @@ export default function HomePage({events}) {
       {events.map(evt => (
         <EventItem key={evt.id} evt={evt} />
       ))}
+
+      <Pagination page={page} total={total} />
+
    </Layout>
   )
 }
 
-export async function getStaticProps() {
-  const res = await fetch(`${API_URL}/events?_sort=date:ASC`)
-  const events = await res.json()
+export async function getServerSideProps({ query: { page = 1 } }) {
+  
+  // Calculate start page
+  const start = +page === 1 ? 0 : (+page - 1) * PER_PAGE
+
+  // Fetch total number of events
+  const totalRes = await fetch(`${API_URL}/events/count`)
+  const total = await totalRes.json()
+  
+  // Fetch events
+  const eventRes = await fetch(`${API_URL}/events?_sort=date:ASC&_limit=${PER_PAGE}&_start=${start}`)
+  const events = await eventRes.json()
 
   return {
-    props: { events },
-    revalidate: 1 // If the data is changed then update after 1 second
+    props: { events, page: +page, total },
   }
 }
 
