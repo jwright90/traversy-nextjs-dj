2538374854a2ea0fee42e2e74f750c5969113bb5
jwright90 <jason.wright90@gmail.com>
2021-11-13 16:46:58 +0000

    prepare to deploy

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -48,28 +48,39 @@ export default function EventPage({evt}) {
   )
 }
 
-export async function getStaticPaths() {
-  const res = await fetch(`${API_URL}/events`)
-  const events = await res.json()
-  const paths = events.map(evt => ({
-    params: {slug: evt.slug}
-  }))
-  return {
-    paths,
-    fallback: true
- }
-}
+// export async function getStaticPaths() {
+//   const res = await fetch(`${API_URL}/events`)
+//   const events = await res.json()
+//   const paths = events.map(evt => ({
+//     params: {slug: evt.slug}
+//   }))
+//   return {
+//     paths,
+//     fallback: true
+//  }
+// }
  
-export async function getStaticProps({ params: { slug }}) {
+// export async function getStaticProps({ params: { slug }}) {
 
+//   const res = await fetch(`${API_URL}/events?slug=${slug}`)
+//   const events = await res.json()
+
+//   return {
+//     props: {
+//       evt: events[0]
+//     },
+//     revalidate: 1,
+//   }
+// }
+
+export async function getServerSideProps({ query: { slug } }) {
   const res = await fetch(`${API_URL}/events?slug=${slug}`)
   const events = await res.json()
 
   return {
     props: {
-      evt: events[0]
+      evt: events[0],
     },
-    revalidate: 1,
   }
 }
 
